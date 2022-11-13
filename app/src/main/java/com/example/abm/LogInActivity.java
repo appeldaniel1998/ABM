a953daee7093e16ebd1123ad9ed3849c94b105c2
Daniel Appel <appel.daniel1998@gmail.com>
2022-11-13 15:21:47 +0200

    Added login functionality

--- app/src/main/java/com/example/abm/LogInActivity.java
+++ app/src/main/java/com/example/abm/LogInActivity.java
@@ -1,14 +1,59 @@
 package com.example.abm;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LogInActivity extends AppCompatActivity {
+
+    private EditText email;
+    private EditText password;
+    private Button logIn;
+
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        logIn = findViewById(R.id.logInButton);
+
+        auth = FirebaseAuth.getInstance();
+
+        logIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String textEmail = email.getText().toString();
+                String textPassword = password.getText().toString();
+                loginUser(textEmail, textPassword);
+            }
+        });
+    }
+
+    private void loginUser(String email, String password) {
+        auth.signInWithEmailAndPassword(email, password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
+                Toast.makeText(LogInActivity.this, "Login successful", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(LogInActivity.this, MainActivity.class));
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/abm/RegisterActivity.java
+++ app/src/main/java/com/example/abm/RegisterActivity.java
@@ -3,6 +3,7 @@ package com.example.abm;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
@@ -57,6 +58,8 @@ public class RegisterActivity extends AppCompatActivity {
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if (task.isSuccessful()) {
                     Toast.makeText(RegisterActivity.this, "Registration was successful", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(RegisterActivity.this, MainActivity.class));
+                    finish();
                 } else {
                     Toast.makeText(RegisterActivity.this, "Registration failed", Toast.LENGTH_SHORT).show();
                 }
