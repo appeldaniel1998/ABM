fa038ce0db6d81acb924a504c3851c2225059645
yuvalTrip <77538019+yuvalTrip@users.noreply.github.com>
2022-12-09 11:21:24 +0200

    Time picker and connection to edit cell finished.

--- app/src/main/java/com/example/abm/Appointments/AppointmentCalender/EventEditActivity.java
+++ app/src/main/java/com/example/abm/Appointments/AppointmentCalender/EventEditActivity.java
@@ -2,14 +2,18 @@ package com.example.abm.Appointments.AppointmentCalender;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import com.example.abm.R;
 
 import java.time.LocalTime;
+import java.util.Locale;
 
 public class EventEditActivity extends AppCompatActivity {
     ////Activity_appointments_calender_event_edit
@@ -17,7 +21,8 @@ public class EventEditActivity extends AppCompatActivity {
     private EditText eventNameET;//event Name Edit Text
     private TextView eventDateTV, eventTimeTV; //2 text views
     private LocalTime time;
-
+    Button timeButton;//time picker
+    int hour, minute;//time picker
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +32,7 @@ public class EventEditActivity extends AppCompatActivity {
         time=LocalTime.now();/////need to change!
         eventDateTV.setText("Date: "+CalenderUtils.formatteDate(CalenderUtils.selectedDate));//defined the date to be the date that the user selected
         eventTimeTV.setText("Time: "+CalenderUtils.formatteTime(time));//defined the time to be the time that the user selected
+        timeButton=findViewById(R.id.timeButton);
 
 
     }
@@ -45,5 +51,27 @@ public class EventEditActivity extends AppCompatActivity {
         Event newEvent=new Event (eventName, CalenderUtils.selectedDate, time);
         Event.eventsList.add(newEvent);
         finish();//close the activity
+    }
+
+    public void popTimePicker(View view)
+    {
+        TimePickerDialog.OnTimeSetListener onTimeSetListener = new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute)
+            {
+                hour = selectedHour;
+                minute = selectedMinute;
+                timeButton.setText(String.format(Locale.getDefault(), "%02d:%02d",hour, minute));
+                eventTimeTV.setText("Time: "+String.format(Locale.getDefault(), "%02d:%02d",hour, minute));//defined the time to be the time that the user selected
+
+            }
+        };
+
+        // int style = AlertDialog.THEME_HOLO_DARK; //different style of picker uf we want to change
+
+        TimePickerDialog timePickerDialog = new TimePickerDialog(this, /*style,*/ onTimeSetListener, hour, minute, true);
+
+        timePickerDialog.setTitle("Select Time");
+        timePickerDialog.show();
     }
 }
--- app/src/main/java/com/example/abm/Appointments/AppointmentCalender/TimePickerMain.java
+++ app/src/main/java/com/example/abm/Appointments/AppointmentCalender/TimePickerMain.java
@@ -14,41 +14,23 @@ import android.widget.TimePicker;
 
 import java.util.Locale;
 
-public class TimePickerMain extends AppCompatActivity {
+public class TimePickerMain {
+//extends AppCompatActivity {
 ////Activity_appointments_calender_time_picker
 
     // The links I used:
     //https://www.youtube.com/watch?v=c6c1giRekB4&t=0s
     //https://github.com/codeWithCal/TimePickerAndroidStudio/blob/master/app/src/main/res/layout/activity_main.xml
     //
-    Button timeButton;
-    int hour, minute;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_appointments_calender_time_picker);
-        timeButton = findViewById(R.id.timeButton);
-    }
 
-    public void popTimePicker(View view)
-    {
-        TimePickerDialog.OnTimeSetListener onTimeSetListener = new OnTimeSetListener() {
-            @Override
-            public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute)
-            {
-                hour = selectedHour;
-                minute = selectedMinute;
-                timeButton.setText(String.format(Locale.getDefault(), "%02d:%02d",hour, minute));
-            }
-        };
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState)
+//    {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_appointments_calender_event_edit);
+//        timeButton = findViewById(R.id.timeButton);
+//    }
 
-        // int style = AlertDialog.THEME_HOLO_DARK; //different style of picker uf we want to change
 
-        TimePickerDialog timePickerDialog = new TimePickerDialog(this, /*style,*/ onTimeSetListener, hour, minute, true);
-
-        timePickerDialog.setTitle("Select Time");
-        timePickerDialog.show();
-    }
 }
