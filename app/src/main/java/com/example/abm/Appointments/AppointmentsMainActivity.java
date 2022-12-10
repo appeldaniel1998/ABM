22fd92fa310c8e3a201b10db36aedd669d4389f8
Daniel Appel <appel.daniel1998@gmail.com>
2022-12-10 18:26:58 +0200

    tweak

--- app/src/main/java/com/example/abm/Appointments/AppointmentsMainActivity.java
+++ app/src/main/java/com/example/abm/Appointments/AppointmentsMainActivity.java
@@ -72,6 +72,7 @@ public class AppointmentsMainActivity extends BaseActivity {
                                                                 }
                                                             }
                                                             progressDialog.dismiss();
+
                                                         });
                                             }
                                         });
